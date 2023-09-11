cdda643773aea18b37d37510125f4512ad45d482
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:11:53 -0400

    AI-enhanced and auto-formatted code

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import time
 def fibonacci(n):
     if n <= 0:
         return "Input should be a positive integer."
-
     if n == 1:
         return 0
-
     if n == 2:
         return 1
 
@@ -15,18 +13,17 @@ def fibonacci(n):
     second = 1
 
     for _ in range(2, n):
-        temp = first
-        first = second
-        second = temp + second
+        first, second = second, first + second
 
     return second
 
 
-start_time = time.time()
-result = fibonacci(10)
-end_time = time.time()
+if __name__ == "__main__":
+    start_time = time.time()
+    result = fibonacci(10)
+    end_time = time.time()
 
-execution_time = end_time - start_time
+    execution_time = end_time - start_time
 
-print(f"The 10th Fibonacci number is: {result}")
-print(f"Execution time: {execution_time} seconds")
+    print(f"The 10th Fibonacci number is: {result}")
+    print(f"Execution time: {execution_time} seconds")
